65fc4a7f14c85c7d1041c4290f01d6d860b16192
Alessandro Trincone <96735976+Trincalex@users.noreply.github.com>
2024-04-10 07:57:11 +0200

    AF

---
+++ Progetto/activation_function.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def sigmoid(x,der=0):
+    y=1/(1+np.exp(x))
+    if der==0:
+        return y
+    else:
+        return y*(1-y)
+
+def tanh(x,der=0):
+    a=np.exp(2*x)
+    y=(a-1)/(a+1)
+    if der==0:
+        return y
+    else:
+        return 1-y*y
+    
+def identity(x,der=0):
+    if der==0:
+        return x
+    else:
+        return 1
